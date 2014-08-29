f97d3561534f95706639145632ce6ccef3383d45
whizzball1 <jonathan.spirit@yahoo.com>
2014-08-29 08:37:33 -0700

    Update ButtonFunctions.js

--- JSCode/ButtonFunctions.js
+++ JSCode/ButtonFunctions.js
@@ -2,3 +2,7 @@ function upQuarkClick() {
   player.clicks.UQ += player.clickValues.UQ;
   update_clicks(player);
 }
+function electronClick() {
+  player.clicks.electron += player.clickValues.electron;
+  update_clicks(player);
+}
