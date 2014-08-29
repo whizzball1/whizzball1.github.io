475f7f97a1c6c7a123fef818b9126403de497ae4
whizzball1 <jonathan.spirit@yahoo.com>
2014-08-28 19:33:03 -0700

    Update Update.js

--- JSCode/Update.js
+++ JSCode/Update.js
@@ -1,7 +1,9 @@
 function gameLoop(player) {
-  clicks = player.clicks;
-  autos = player.autoClicks;
-  update_clicks(player);
+  while true {
+    clicks = player.clicks;
+    autos = player.autoClicks;
+    update_clicks(player);
+  }
 };
 function update_clicks(player) {
   clicks = player.clicks;
