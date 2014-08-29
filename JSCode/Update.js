155db24dfb36afc3a8843fbb21f3eb47bb0b85fe
whizzball1 <jonathan.spirit@yahoo.com>
2014-08-28 20:06:13 -0700

    Update Update.js

--- JSCode/Update.js
+++ JSCode/Update.js
@@ -1,8 +1,9 @@
+window.onload = function() {
 function gameLoop(player) {
   clicks = player.clicks;
   autos = player.autoClicks;
   update_clicks(player);
-};
+}
 function update_clicks(player) {
   clicks = player.clicks;
   document.getElementById("total_up_quark_clicks").innerHTML = clicks.UQ;
@@ -10,4 +11,5 @@ function update_clicks(player) {
 var game = true;
 while (game === true) {
   gameLoop(player);
+}
 }
