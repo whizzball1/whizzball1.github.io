3e1b6d207de41abcaa469b5661ddfe406577c0ad
whizzball1 <jonathan.spirit@yahoo.com>
2014-08-28 18:52:53 -0700

    Update TemporaryName.js

--- JSCode/TemporaryName.js
+++ JSCode/TemporaryName.js
@@ -1,1 +1,16 @@
-stjff
+var player = {
+  clicks: {
+    upQuark: 0,
+    downQuark: 0,
+    electron: 0,
+    proton: 0,
+    neutron: 0
+  },
+  autoClicks: {
+    
+  }
+};
+function gameLoop(player) {
+  clicks = player.clicks;
+  autos = player.autoClicks;
+};
