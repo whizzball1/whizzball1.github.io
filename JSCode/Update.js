285af5103f4f999460a73c3676ae0fceac9a90bd
whizzball1 <jonathan.spirit@yahoo.com>
2014-08-30 07:58:32 -0700

    Update Update.js

--- JSCode/Update.js
+++ JSCode/Update.js
@@ -8,6 +8,8 @@ function update_clicks(player) {
   document.getElementById("total_up_quark_clicks").innerHTML = clicks.UQ;
   document.getElementById("total_electron_clicks").innerHTML = clicks.electron;
   document.getElementById("total_down_quark_clicks").innerHTML = clicks.DQ;
+  document.getElementById("total_protons").innerHTML = clicks.proton;
+  document.getElementById("total_neutrons").innerHTML = clicks.neutron;
 }
 var game = true;
 setInterval(function () {
