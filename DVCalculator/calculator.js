aa6c16644a12e4463016e7dc96d481bf3db2900f
whizzball1 <jonathan.spirit@yahoo.com>
2016-08-15 13:34:09 -0700

    Update calculator.js

--- DVCalculator/calculator.js
+++ DVCalculator/calculator.js
@@ -368,6 +368,8 @@ function save() {
   save["boosts"]["earth"] = document.getElementById("earth_row").childNodes[3].innerHTML
   save["boosts"]["light"] = document.getElementById("light_row").childNodes[3].innerHTML
   save["boosts"]["dark"] = document.getElementById("dark_row").childNodes[3].innerHTML
+  
+  save["groups"] = {}
   for (i = 0; i < groups.length; i++) {
     currentGroup = groups[i];
     save["groups"][currentGroup] = {}
